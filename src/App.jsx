79b3902aeccb820f9dce96fbae282c40c5c68a84
Goddii <otienogodwin23@gmail.com>
2026-06-04 18:56:41 +0300

    initial commit of files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+  const [scrolled, setScrolled] = 
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
