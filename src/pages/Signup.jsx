e9c4cc83392b40819d05670144b7027ded544f99
Goddii <otienogodwin23@gmail.com>
2026-06-07 00:23:06 +0300

    feat: add state to signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,24 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import { useState } from "react";
 
 
 
 export default function Signup() {
-    
+    const [searchParams] = useSearchParams();
+
+    //select role if coming from hero buttons
+    const [role, setRole] = useState(searchParams.get("role") || "buyer");
+
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    });
+
+    const [error, setError] = useState('')
+    const navigate = useNavigate();
+
+    func
+
 }
