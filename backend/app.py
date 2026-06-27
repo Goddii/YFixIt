f3d79b8001047583345d41795a2845b97a0be3f7
Goddii <otienogodwin23@gmail.com>
2026-06-27 19:22:22 +0300

    fix app.py for  gunicorn compatibility

--- backend/app.py
+++ backend/app.py
@@ -75,8 +75,10 @@ def create_app():
         db.create_all()
 
     return app
+    
+app = create_app()    
 
 if __name__ == "__main__":
-    app = create_app()
+    
     app.run(debug=True, port=8000)
 
